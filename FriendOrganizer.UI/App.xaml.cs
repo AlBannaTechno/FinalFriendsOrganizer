ea974c69a488b6b747740cc2a4e4df1bea8f05d1
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 14:51:59 +0200

    [04. Building the Basic UI Layer]:[Using Bootstrapper to resolve the main window and run the project]

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Autofac;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Startup;
 using FriendOrganizer.UI.ViewModel;
 
 namespace FriendOrganizer.UI
@@ -17,11 +19,9 @@ namespace FriendOrganizer.UI
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            MainWindow mainWindow=new MainWindow(
-                new MainViewModel(
-                    new FriendDataService()
-                    )
-                );
+            var bootstrapper = new BootStrapper();
+            var container = bootstrapper.Bootstrap();
+            var mainWindow = container.Resolve<MainWindow>();
             mainWindow.Show();
         }
     }
