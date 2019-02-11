8182cdbde5bb2fef6ec4e30a48ccb247e1b5e2e7
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 14:07:04 +0200

    [04. Building the Basic UI Layer]:[Create MainViewModel With Basic Functionalites]
    We Create Friends property and Load Method
    Also pass IFriendDataService to the constructor to support DI
    Also use ObservableCollections

---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    class MainViewModel
+    {
+        private IFriendDataService _friendDataService;
+        private Friend _selectedFrien;
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends=new ObservableCollection<Friend>();
+            _friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (Friend friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFrien; }
+            set { _selectedFrien = value; }
+        }
+    }
+}
