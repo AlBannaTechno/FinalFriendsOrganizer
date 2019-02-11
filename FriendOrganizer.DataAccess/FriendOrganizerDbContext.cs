3837cfc0ef5a7a6882e4f2f19049d30c0bd5f15a
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 16:23:15 +0200

    [05. Setting up Entity Framework]:[Add FriendOrganizerDbContext Class To DataAcess Project]

---
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.DataAccess
+{
+    class FriendOrganizerDbContext:DbContext
+    {
+        // https://stackoverflow.com/questions/30084916/no-connection-string-named-could-be-found-in-the-application-config-file/37697318
+        // using :base(name=FriendOrganizerDb") only allowable if
+        // 1- we set ConnectionString in The StartUp Project Configs
+        // 2- or we mark current project as startUp Project
+        // Otherwise we must use :base("FriendOrganizerDb") before execute enable-migrations
+        public FriendOrganizerDbContext()
+            :base("FriendOrganizerDb")
+        {
+            
+        }
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
