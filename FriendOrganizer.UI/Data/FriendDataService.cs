a8fa53291525147664f1c1777e18d25754bbe6df
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 17:23:54 +0200

    [05. Setting up Entity Framework]:[Using DI with FriendDataService]
    This done with two steps
    1-Regiestring FriendOrganizerDbContext int Bootstrapper
    2-Pass FriendOrganizerDbContext to the constructor of FriendDataService

--- FriendOrganizer.UI/Data/FriendDataService.cs
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using FriendOrganizer.DataAccess;
 using FriendOrganizer.Model;
@@ -7,9 +8,51 @@ namespace FriendOrganizer.UI.Data
 {
     class FriendDataService : IFriendDataService
     {
+        /* Important Explanation
+         *
+         * we can use : Next : 
+            private readonly FriendOrganizerDbContext _contextCreator;
+             
+             public FriendDataService(FriendOrganizerDbContext contextCreator)
+                {
+                    _contextCreator = contextCreator;
+                }
+            public IEnumerable<Friend> GetAll()
+            {
+                using (var ctx = _contextCreator)
+                {
+                    return ctx.Friends.AsNoTracking().ToList();
+                }
+            }
+
+            But If we need to Support Create instance of FriendOrganizerDbContext inside Autofac
+            We Must use the current => [use it inside a function to force autofac to create new instance every time]
+                -------
+                Benefit of doing that :
+                    we can use that to allow autofac to use just one instance with every time we call GetAll()
+                    this mean all thoses data will be in the same context
+                    [This will be by changing BootStrapper.cs]
+                        builder.RegisterType<FriendOrganizerDbContext>().AsSelf().SingleInstance();
+                    But now we just create new instance every once we call GetAll() method
+                    and this is impossible if we pass FriendOrganizerDbContext to the constructor 
+                    Because autofac will just create the instance once
+                Finally we can say
+                Using FriendOrganizerDbContext directly
+                    =
+                        Func<FriendOrganizerDbContext> : this mean function return FriendOrganizerDbContext
+                        +
+                        builder.RegisterType<FriendOrganizerDbContext>().AsSelf().SingleInstance();
+                        
+         */
+
+        private readonly Func<FriendOrganizerDbContext> _contextCreator;
+        public FriendDataService(Func<FriendOrganizerDbContext> contextCreator)
+        {
+            _contextCreator = contextCreator;
+        }
         public IEnumerable<Friend> GetAll()
         {
-            using (var ctx=new FriendOrganizerDbContext())
+            using (var ctx = _contextCreator())
             {
                 return ctx.Friends.AsNoTracking().ToList();
             }
--- FriendOrganizer.UI/Startup/BootStrapper.cs
+++ FriendOrganizer.UI/Startup/BootStrapper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autofac;
+using FriendOrganizer.DataAccess;
 using FriendOrganizer.UI.Data;
 using FriendOrganizer.UI.ViewModel;
 
@@ -17,6 +18,7 @@ namespace FriendOrganizer.UI.Startup
             builder.RegisterType<MainWindow>().AsSelf();
             builder.RegisterType<MainViewModel>().AsSelf();
             builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+            builder.RegisterType<FriendOrganizerDbContext>().AsSelf();
 
             return builder.Build();
         }
