014fe0060b6d5ae349c9e4e2961ce5498616dc82
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 16:31:48 +0200

    [05. Setting up Entity Framework]:[Prepare Seed Method to populating friend table with data]

--- FriendOrganizer.DataAccess/Migrations/Configuration.cs
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using FriendOrganizer.Model;
+
 namespace FriendOrganizer.DataAccess.Migrations
 {
     using System;
@@ -14,10 +16,13 @@ namespace FriendOrganizer.DataAccess.Migrations
 
         protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            context.Friends.AddOrUpdate(
+                f=>f.FirstName,
+                new Friend() { FirstName = "Osama", LastName = "AlBanna", Email = "Al_Banna_Tehno@yahoo.com", Id = 1 },
+                new Friend() { FirstName = "Nour", LastName = "Sad", Email = "Nour@yahoo.com", Id = 2 },
+                new Friend() { FirstName = "Omar", LastName = "Fekry", Email = "OmarF@yahoo.com", Id = 3 },
+                new Friend() { FirstName = "Rihcodo", LastName = "Rich", Email = "RihRich@yahoo.com", Id = 4 }
+               );
         }
     }
 }
