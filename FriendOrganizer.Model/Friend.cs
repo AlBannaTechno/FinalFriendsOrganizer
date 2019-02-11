c716bf75d6cdba88c9e9d2a2dfd9e7fc23808fd5
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 16:46:25 +0200

    [05. Setting up Entity Framework]:[Set constraints With DataAnnotations]

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -1,10 +1,16 @@
-﻿namespace FriendOrganizer.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FriendOrganizer.Model
 {
     public class Friend
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
