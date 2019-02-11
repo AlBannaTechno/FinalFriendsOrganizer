a490867da5a0cbf328a0cd1eecbc532bab244b08
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 13:52:23 +0200

    [03. Defining the Architecture]:[Fix typing I->Id]

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -2,7 +2,7 @@
 {
     public class Friend
     {
-        public int I { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
