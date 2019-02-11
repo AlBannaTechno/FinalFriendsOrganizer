378400779234c903caf8f89b8d2b0c74adc6d2d0
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 20:47:15 +0200

    [06. Decoupling the UI Parts]:[Create FriendDetailViewModel]

---
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
+    {
+        private readonly IFriendDataService _dataService;
+
+        public FriendDetailViewModel(IFriendDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public async Task LoadAsync(int friendId)
+        {
+            Friend = await _dataService.GetByIdAsync(friendId);
+        }
+
+        private Friend _friend;
+
+        public Friend Friend
+        {
+            get => _friend;
+            set
+            {
+                _friend = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/IFriendDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public interface IFriendDetailViewModel
+    {
+        Task LoadAsync(int friendId);
+    }
+}
