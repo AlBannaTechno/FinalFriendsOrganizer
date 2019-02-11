2de8b4a15165b158d73f39d72e7d6627f75f24a5
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 14:49:52 +0200

    [04. Building the Basic UI Layer]:[Create BootStrapper class to use autofac -> and registering current types]

---
+++ FriendOrganizer.UI/Startup/BootStrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI.Startup
+{
+    public class BootStrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder=new ContainerBuilder();
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+
+            return builder.Build();
+        }
+    }
+}
