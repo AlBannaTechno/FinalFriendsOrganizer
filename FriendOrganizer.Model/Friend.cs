56264b46a6640896fe08576cf53374feb6470f00
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 13:19:18 +0200

    [03. Defining the Architecture]:[Define Friend Model Properites]

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -2,5 +2,9 @@
 {
     public class Friend
     {
+        public int I { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
