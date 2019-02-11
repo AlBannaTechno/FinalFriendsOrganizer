b4cab9bdf43fad61acfd2c0b82e49d5dbe8db505
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 20:08:17 +0200

    [06. Decoupling the UI Parts]:[Create a NavigationViewModel]

---
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    class NavigationViewModel
+    {
+        private readonly IFriendLookupDataService _friendLookupService;
+
+        public NavigationViewModel(IFriendLookupDataService friendLookupService)
+        {
+            _friendLookupService = friendLookupService;
+            Friends=new ObservableCollection<LookupItem>();
+        }
+
+        public async Task LoadAsync()
+        {
+            var lookup =await _friendLookupService.GetFriendLookupAsync();
+            Friends.Clear();
+            foreach (LookupItem friend in lookup)
+            {
+                Friends.Add(friend);
+            }
+        }
+        public ObservableCollection<LookupItem> Friends { get; set; }
+    }
+}
