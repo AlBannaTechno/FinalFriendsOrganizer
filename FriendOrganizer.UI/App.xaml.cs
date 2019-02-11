0dc663ae4cc5e396ee56d53c2b3ae8703255a62a
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-11 14:40:55 +0200

    [04. Building the Basic UI Layer]:[Modify App.cs/xaml to load MainWindow From Loaded Event ]
    The purpose of that to support DI

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
 
 namespace FriendOrganizer.UI
 {
@@ -13,5 +15,14 @@ namespace FriendOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow=new MainWindow(
+                new MainViewModel(
+                    new FriendDataService()
+                    )
+                );
+            mainWindow.Show();
+        }
     }
 }
