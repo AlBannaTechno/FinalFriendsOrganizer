b07938d34a15be514ac9e34728752eb3036d79d5
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-12 10:15:13 +0200

    [07. Communicating Between ViewModels]:[Prepare FriendDetailViewModel To Use OpenFriendDetailViewEvent and subscribe to it [Finish]]

--- FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -5,24 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
     class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
     {
         private readonly IFriendDataService _dataService;
+        private Friend _friend;
+        private readonly IEventAggregator _eventAggregator;
 
-        public FriendDetailViewModel(IFriendDataService dataService)
+        public FriendDetailViewModel(IFriendDataService dataService,IEventAggregator eventAggregator)
         {
             _dataService = dataService;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                .Subscribe(OnOpenFriendDetailView);
+        }
+
+        private async void OnOpenFriendDetailView(int friendId)
+        {
+            await LoadAsync(friendId);
         }
 
         public async Task LoadAsync(int friendId)
         {
             Friend = await _dataService.GetByIdAsync(friendId);
         }
-
-        private Friend _friend;
 
         public Friend Friend
         {
