3052faca95a53d0a3b26fec281e7811fe6eee9aa
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-12 10:48:09 +0200

    [08. Saving Data]:[create AfterFriendSavedEvent]

---
+++ FriendOrganizer.UI/Event/AfterFriendSavedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Events;
+
+namespace FriendOrganizer.UI.Event
+{
+    public class AfterFriendSavedEvent:PubSubEvent<AfterFriendSavedEventArgs>
+    {
+    }
+
+    public class AfterFriendSavedEventArgs
+    {
+        public int Id { get; set; }
+        public string DisplayMember { get; set; }
+    }
+}
