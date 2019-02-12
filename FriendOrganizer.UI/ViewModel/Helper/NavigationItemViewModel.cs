fb994863dfb27a414018bc320439b8ed8844cfc3
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-12 11:14:31 +0200

    [08. Saving Data]:[Create NavigationItemViewModel as a helper class instead of using LookupItem class directly]

---
+++ FriendOrganizer.UI/ViewModel/Helper/NavigationItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace FriendOrganizer.UI.ViewModel.Helper
+{
+    /*
+     * This is just a helper class to implement iNotifyPropertyChanged instead of implement it
+     * Inside LookupItem model class
+     * Because we need Models to be fully isolated
+     */
+    public class NavigationItemViewModel:ViewModelBase
+    {
+
+        public NavigationItemViewModel(int id,string displayMember)
+        {
+            Id = id;
+            DisplayMember = displayMember;
+        }
+        public int Id { get; }
+        private string _displayMember;
+
+        public string DisplayMember
+        {
+            get => _displayMember;
+            set
+            {
+                _displayMember = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
