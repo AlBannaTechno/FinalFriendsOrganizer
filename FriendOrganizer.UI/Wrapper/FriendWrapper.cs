469224263654e517d49e741e274437db7932876f
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-12 12:44:23 +0200

    [09. Validating User Input]:[create FriendWrapper to using it instead of Friend model class to implement validation in it]

---
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -0,0 +1,46 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class FriendWrapper:ViewModelBase
+    {
+        public FriendWrapper(Friend model)
+        {
+            Model = model;
+        }
+        public Friend Model { get; }
+
+        public int Id => Model.Id;
+
+        public string FirstName
+        {
+            get => Model.FirstName;
+            set
+            {
+                Model.FirstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => Model.LastName;
+            set
+            {
+                Model.LastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => Model.Email;
+            set
+            {
+                Model.Email = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
