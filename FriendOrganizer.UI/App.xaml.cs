7180f316e08abec86851c9946af69ebebd877840
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-12 12:29:49 +0200

    [09. Validating User Input]:[Set Application level exception handler]

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using Autofac;
 using FriendOrganizer.UI.Data;
 using FriendOrganizer.UI.Startup;
@@ -23,6 +24,12 @@ namespace FriendOrganizer.UI
             var container = bootstrapper.Bootstrap();
             var mainWindow = container.Resolve<MainWindow>();
             mainWindow.Show();
+        }
+
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show(e.Exception.Message.ToString(), "Exception");
         }
     }
 }
