dc79d65bc679b7ddac264d5642d34d4e80076b3f
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-12 13:22:22 +0200

    [09. Validating User Input]:[implement INotifyDataErrorInfo inside FriendWrapper]

--- FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,9 +1,14 @@
-﻿using FriendOrganizer.Model;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using FriendOrganizer.Model;
 using FriendOrganizer.UI.ViewModel;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-    public class FriendWrapper:ViewModelBase
+    public class FriendWrapper:ViewModelBase,INotifyDataErrorInfo
     {
         public FriendWrapper(Friend model)
         {
@@ -40,6 +45,46 @@ namespace FriendOrganizer.UI.Wrapper
             {
                 Model.Email = value;
                 OnPropertyChanged();
+            }
+        }
+
+        private readonly Dictionary<string,List<string>> _errorsBtPropertyName=
+            new Dictionary<string, List<string>>();
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _errorsBtPropertyName.ContainsKey(propertyName) ?
+                _errorsBtPropertyName[propertyName] :
+                null;
+        }
+        public bool HasErrors => _errorsBtPropertyName.Any();
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        private void OnErrorChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this,new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        public void AddError(string propertyName, string error)
+        {
+            if (!_errorsBtPropertyName.ContainsKey(propertyName))
+            {
+                _errorsBtPropertyName[propertyName]=new List<string>();
+            }
+
+            if (!_errorsBtPropertyName[propertyName].Contains(error))
+            {
+                _errorsBtPropertyName[propertyName].Add(error);
+                OnErrorChanged(propertyName);
+            }
+        }
+
+        public void ClearError(string propertyName)
+        {
+            if (_errorsBtPropertyName.ContainsKey(propertyName))
+            {
+                _errorsBtPropertyName.Remove(propertyName);
+                OnErrorChanged(propertyName);
             }
         }
     }
