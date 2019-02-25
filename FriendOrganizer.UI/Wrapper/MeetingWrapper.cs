6689d7652267a36ff6c4eff1b5289cfafd8d9855
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-25 10:28:48 +0200

    [15. Introducing Another Detail View] :  Create MeetingWrapper

---
+++ FriendOrganizer.UI/Wrapper/MeetingWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    class MeetingWrapper:ModelWrapper<Meeting>
+    {
+        public MeetingWrapper(Meeting model) : base(model)
+        {
+        }
+
+        public int Id => Model.Id;
+
+        public string Title
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
+        public DateTime DateFrom
+        {
+            get => GetValue<DateTime>();
+            set => SetValue(value);
+        }
+
+        public DateTime DateTo
+        {
+            get => GetValue<DateTime>();
+            set => SetValue(value);
+        }
+    }
+}
