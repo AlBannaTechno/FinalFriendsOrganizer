fd70bc148f69b88b5a48eb29aca83270d24918b5
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-25 10:19:49 +0200

    [15. Introducing Another Detail View] : Create Meeting Repository

---
+++ FriendOrganizer.UI/Data/Repositories/IMeetingRepository.cs
@@ -0,0 +1,8 @@
+﻿using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data.Repositories
+{
+    public interface IMeetingRepository:IGenericRepository<Meeting>
+    {
+    }
+}
---
+++ FriendOrganizer.UI/Data/Repositories/MeetingRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using FriendOrganizer.DataAccess;
+using FriendOrganizer.Model;
+using System.Data.Entity;
+namespace FriendOrganizer.UI.Data.Repositories
+{
+    public class MeetingRepository:GenericRepository<Meeting,FriendOrganizerDbContext>, IMeetingRepository
+    {
+        protected MeetingRepository(FriendOrganizerDbContext context) : base(context)
+        {
+        }
+
+        public override async Task<Meeting> GetByIdAsync(int id)
+        {
+            return await Context.Meetings.Include(m => m.Friends).SingleAsync(m=>m.Id==id);
+        }
+    }
+}
