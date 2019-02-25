7757a4604cf777a2d97063ad0b7b8c54fcebec05
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2019-02-25 16:12:23 +0200

    [15. Introducing Another Detail View] : sync start/end date

--- FriendOrganizer.UI/Wrapper/MeetingWrapper.cs
+++ FriendOrganizer.UI/Wrapper/MeetingWrapper.cs
@@ -24,13 +24,27 @@ namespace FriendOrganizer.UI.Wrapper
         public DateTime DateFrom
         {
             get => GetValue<DateTime>();
-            set => SetValue(value);
+            set
+            {
+                SetValue(value);
+                if (DateTo < DateFrom)
+                {
+                    DateTo = DateFrom;
+                }
+            }
         }
 
         public DateTime DateTo
         {
             get => GetValue<DateTime>();
-            set => SetValue(value);
+            set
+            {
+                SetValue(value);
+                if (DateTo<DateFrom)
+                {
+                    DateFrom = DateTo;
+                }
+            }
         }
     }
 }
